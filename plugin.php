feb9ac8f1cf3510b15cb8112709286bb296aca73
Imbar Budiman <imbarbudiman@yahoo.com>
2018-09-28 17:27:10 +0700

    change url & plugin name

--- plugin.php
+++ plugin.php
@@ -3,11 +3,11 @@
 /**
  * @wordpress-plugin
  * Plugin Name:       Herbert Map Shortcode
- * Plugin URI:        http://plugin-name.com/
+ * Plugin URI:        https://github.com/imbarbudiman/herbert-map-short
  * Description:       Simple wordpress shortcode plugins, to display the google map in your posts with coordinates (latitude and longitude).
  * Version:           1.0.0
  * Author:            Imbar Budiman
- * Author URI:        http://author.com/
+ * Author URI:        https://github.com/imbarbudiman
  * License:           MIT
  */
 
